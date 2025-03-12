742c4924f0e51be457cd809409868c23d993c601
theBappy <abappy575@gmail.com>
2025-03-12 19:29:29 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Downloads from "./sections/Downloads"
+import Faq from "./sections/Faq"
+import Features from "./sections/Features"
+import Footer from "./sections/Footer"
+import Header from "./sections/Header"
+import Hero from "./sections/Hero"
+import Pricing from "./sections/Pricing"
+import Testimonials from "./sections/Testimonials"
+
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Downloads />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
